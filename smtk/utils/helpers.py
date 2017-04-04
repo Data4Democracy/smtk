56494211ed37b5b3858debeca659a589c15ca47b
Alejandro Huerta <ahuerta0686@gmail.com>
2017-04-04 07:24:41 -0500

    WIP Minimum working Reddit collector (#9)
    * Reddit auth and redditor methods
    
    * Authenticate with reddit
    
    * Reddit: get link karma method
    
    * Abstracted redditor fetch
    
    * Fixed call to fetch redditors
    
    * Fixed formatting issues
    
    * Semicolons removed in reddit.py
    
    * Newline added in PipFile
    
    * Add get_submissions function to CollectReddit
    
    * Added more redditor methods
    
    * More closely follows structure of CollectTwitter
    
    + Added get_comments method that can take either ids or usernames
    + Added get_redditors method, takes usernames
    - Removed methods to get individual redditor properties

--- smtk/utils/helpers.py
+++ smtk/utils/helpers.py
@@ -46,3 +46,28 @@ def facebook_auth(app_id=None, app_secret=None):
     auth_token = facepy.utils.get_application_access_token(*auth)
 
     return auth_token
+
+def reddit_auth(client_id=None, client_secret=None):
+    """Validate and create reddit access credentials"""
+    if not client_id:
+        client_id = os.environ.get('R_CLIENT_ID', client_id)
+    if not client_secret:
+        client_secret = os.environ.get('R_CLIENT_SECRET', client_secret)
+
+    user_agent = 'smtk:d4d';
+    # auth = [client_id, client_secret, user_agent]
+    auth = dict(
+        client_id=client_id,
+        client_secret=client_secret,
+        user_agent=user_agent
+    )
+
+    for credential in auth.values():
+        if not isinstance(credential, str):
+            print('credential must be string')
+            return None
+        if len(credential) <= 0:
+            print('invalid credential {}'.format(credential))
+            return None
+
+    return auth
